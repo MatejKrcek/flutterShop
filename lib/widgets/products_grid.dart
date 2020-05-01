2d46a44a708423c65e3e1fd4919596972e44ed88
Matej Krcek <matejkrcek@Jirka-Mac16.local>
2020-05-01 14:01:56 +0200

    favorite fun

--- lib/widgets/products_grid.dart
+++ lib/widgets/products_grid.dart
@@ -5,19 +5,20 @@ import './product_item.dart';
 import '../providers/products.dart';
 
 class ProductsGrid extends StatelessWidget {
-
-
   @override
   Widget build(BuildContext context) {
-    final productsData = Provider.of<Products>(context);   //listen for -Products-
+    final productsData = Provider.of<Products>(context); //listen for -Products-
     final products = productsData.items;
     return GridView.builder(
       padding: const EdgeInsets.all(10),
       itemCount: products.length,
-      itemBuilder: (ctx, i) => ProductItem(
-        products[i].id,
-        products[i].title,
-        products[i].imageUrl,
+      itemBuilder: (ctx, i) => ChangeNotifierProvider(
+        create: (c) => products[i],
+        child: ProductItem(
+          // products[i].id,
+          // products[i].title,
+          // products[i].imageUrl,
+        ),
       ),
       gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
         crossAxisCount: 2,
