7eb8babe86bcdef42319f5fdc7ebfdb90aab20a7
Matej Krcek <matejkrcek@Jirka-Mac16.local>
2020-05-01 12:32:02 +0200

    provider, listeners

--- lib/screens/product_detail_screen.dart
+++ lib/screens/product_detail_screen.dart
@@ -1,15 +1,22 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import '../providers/products.dart';
 
 class ProductDetailScreen extends StatelessWidget {
   static const routeName = '/product-datail';
 
   @override
   Widget build(BuildContext context) {
-    final productId = ModalRoute.of(context).settings.arguments as String ;
-    
+    final productId = ModalRoute.of(context).settings.arguments as String; //id
+    final loadedProduct = Provider.of<Products>(
+      context,
+      listen: false, //dont want to listen
+    ).findById(productId); //findById method in products.dart
+
     return Scaffold(
       appBar: AppBar(
-        title: Text('title'),
+        title: Text(loadedProduct.title),
       ),
     );
   }
