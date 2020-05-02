dd2e1cb867525f56edf575928618d6bb5cc43d8e
Matej Krcek <matejkrcek@Jirka-Mac16.local>
2020-05-02 12:07:37 +0200

    shopping cart prep

--- lib/screens/products_overview_screen.dart
+++ lib/screens/products_overview_screen.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 import '../widgets/products_grid.dart';
+import '../widgets/badge.dart';
+import '../providers/cart.dart';
 
 enum FilterOptions {
   Favorites,
@@ -43,6 +46,17 @@ class _ProductsOverviewScreenState extends State<ProductsOverviewScreen> {
             ],
             icon: Icon(Icons.more_vert),
           ),
+          Consumer<Cart>(
+            builder: (_, cart, ch) => Badge(
+              child: ch,
+              value: cart.itemCount.toString(),
+            ),
+            child: IconButton( //child outside of Coonsumer, does not rebuild
+              icon: Icon(
+                Icons.shopping_cart,
+              ), onPressed: () {},
+            ),
+          )
         ],
       ),
       body: ProductsGrid(_showOnlyFavorites),
