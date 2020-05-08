841fe77981b7fa07b3581b1cdb6337219769e9e1
Matej Krcek <matejkrcek@Jirka-Mac16.local>
2020-05-08 12:23:55 +0200

    errors

--- lib/providers/products.dart
+++ lib/providers/products.dart
@@ -52,9 +52,9 @@ class Products with ChangeNotifier {
     return _items.firstWhere((prod) => prod.id == id);
   }
 
-  void addProduct(Product product) {
+  Future <void> addProduct(Product product) {
     const url = 'https://fluttershop-1cf44.firebaseio.com/products.json';
-    http
+    return http
         .post(
       url,
       body: json.encode(
@@ -78,6 +78,10 @@ class Products with ChangeNotifier {
       );
       _items.add(newProduct);
       notifyListeners();
+    }).catchError((error) {
+      print(error);
+      throw error;
+
     });
   }
 
